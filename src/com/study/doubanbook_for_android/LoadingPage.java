5f1b85773732eae6f5d4bffa6a0b21d406354738
lipan <478760490@qq.com>
2013-11-02 22:07:57 +0800

    init the project and the jars will be used

---
+++ src/com/study/doubanbook_for_android/LoadingPage.java
@@ -0,0 +1,10 @@
+package com.study.doubanbook_for_android;
+
+import android.app.Activity;
+
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_loading_page)
+public class LoadingPage extends Activity {
+
+}
